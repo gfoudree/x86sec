c13d22d481fd758f97a83b84ba665524426d91a2
Grant Foudree <gfoudree@iastate.edu>
2017-04-30 12:19:13 -0500

    Updated templates and added remaining pages

--- main.py
+++ main.py
@@ -5,5 +5,21 @@ app = Flask(__name__)
 def index():
 	return render_template('index.html')
 
+@app.route('/projects')
+def projects():
+	return render_template('projects.html')
+	
+@app.route('/posts')
+def posts():
+	return render_template('posts.html')
+
+@app.route('/downloads')
+def downloads():
+	return render_template('downloads.html')
+
+@app.route('/about')
+def about():
+	return render_template('about.html')
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', debug=True)
