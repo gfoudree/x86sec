81f7dfd440fc27c55fd9aae52320f13db2da27e1
Grant Foudree <gfoudree@iastate.edu>
2017-05-01 18:02:58 -0500

    Looks just like Flask so far...

---
+++ website/views.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def index(request):
+    return render(request, 'index.html', {'title' : 'Index'})
+
+def projects(request):
+    return render(request, 'projects.html', {'title' : 'Projects'})
+
+def posts(request):
+    return render(request, 'posts.html', {'title' : 'Posts'})
+
+def downloads(request):
+    return render(request, 'downloads.html', {'title' : 'Downloads'})
+
+def about(request):
+    return render(request, 'about.html', {'title' : 'About'})
