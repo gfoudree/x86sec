5d8ac37b53e6fe276ab6958300f0dd21f7e83b53
Grant Foudree <gfoudree@iastate.edu>
2017-05-01 13:03:04 -0500

    Added remaining templates and some security headers

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, make_response
 app = Flask(__name__)
+
+headers = {'X-Frame-Sptions' : 'SAMEORIGIN',
+			'X-XSS-Protection' : '1; mode=block',
+			'X-Content-Type-Options' : 'nosniff',
+			'Content-Security-Policy' : 'default-src \'none\'; style-src *.x86sec.com; font-src https://fonts.googleapis.com; img-src *.x86sec.com;',
+			'Referrer-Policy' : 'same-origin'
+		}
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+	return render_template('index.html'), 200, headers
 
 @app.route('/projects')
 def projects():
 	return render_template('projects.html')
-	
+
 @app.route('/posts')
 def posts():
 	return render_template('posts.html')
