acc54a715a5e5e3e4494e70c60e49757455a5c5c
Grant Foudree <gfoudree@iastate.edu>
2017-05-01 23:28:11 -0500

    Added text to about page and PGP key

--- website/middleware.py
+++ website/middleware.py
@@ -6,6 +6,6 @@ class SecurityHeaders:
         response = self.get_response(request)
         response['X-XSS-Protection'] = '1; mode=block'
         response['X-Content-Type-Options'] = 'nosniff'
-        response['Content-Security-Policy'] = 'default-src \'none\'; style-src *.x86sec.com https://fonts.googleapis.com; font-src https://fonts.googleapis.com; img-src *.x86sec.com;'
+        #response['Content-Security-Policy'] = 'default-src \'none\'; style-src *.x86sec.com https://fonts.googleapis.com; font-src https://fonts.googleapis.com; img-src *.x86sec.com;'
         response['Referrer-Policy'] = 'origin'
         return response
--- website/views.py
+++ website/views.py
@@ -6,7 +6,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html', {'title' : 'Index'})
+    return render(request, 'index.html', {'title' : 'Home'})
 
 def projects(request):
     return render(request, 'projects.html', {'title' : 'Projects'})
@@ -18,4 +18,4 @@ def downloads(request):
     return render(request, 'downloads.html', {'title' : 'Downloads'})
 
 def about(request):
-    return render(request, 'about.html', {'title' : 'About'})
+    return render(request, 'about.html', {'title' : 'About Me'})
