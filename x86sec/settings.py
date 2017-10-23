d26d18a58152527a0d8b1e5c649a053d046289ba
Grant Foudree <gfoudree@iastate.edu>
2017-10-22 22:17:19 -0500

    Fixed typo in header

--- x86sec/settings.py
+++ x86sec/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = secrets.secret_key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.x86sec.com', 'x86sec.com', '127.0.0.1']
+ALLOWED_HOSTS = ['www.x86sec.com', 'x86sec.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
