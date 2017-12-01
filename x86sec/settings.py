7252676d3179da2a6064c56438a5083194164908
Grant Foudree <gfoudree@iastate.edu>
2017-12-01 16:55:13 -0600

    Added about page

--- x86sec/settings.py
+++ x86sec/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/var/www/x86sec.com/x86sec/website/static'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "website/static")
 ]
