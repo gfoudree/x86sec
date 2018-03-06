84f56dd7e0cf14cbe09d297d2202e17b376d8f98
Grant Foudree <gfoudree@iastate.edu>
2018-03-05 22:25:14 -0600

    Added anti-reverse engineering page

--- website/views.py
+++ website/views.py
@@ -8,7 +8,7 @@ from website.models import Project, Post, Download
 # Create your views here.
 def index(request):
     post_objs = Post.objects.all()
-    return render(request, 'index.html', {'title' : 'Home', 'posts' : post_objs})
+    return render(request, 'index.html', {'title' : 'Home', 'posts' : reversed(post_objs)}) #Serve in reverse order
 
 def projects(request):
     project_objs = Project.objects.all()
